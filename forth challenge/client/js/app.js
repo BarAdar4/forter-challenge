71d6deea1b666d6503a73446eb5f373fae750eb7
BarAdar4 <baradar4@gmail.com>
2017-06-19 17:58:18 +0300

    added chat room

--- forth challenge/client/js/app.js
+++ forth challenge/client/js/app.js
@@ -8,13 +8,13 @@ app.factory('dataServ', ['$http',function($http) {
             return $http.get('/data');
         },
 
-        getTry : function() {
-        	return $http.get('/try/dfg');
-		},
-
 		getAnswer : function (question) {
         	return $http.get('/try/' + question)
-        }
+        },
+
+        getData : function() {
+        	return $http.get('/data');
+		}
     }
 }]);
 
@@ -23,7 +23,9 @@ app.controller('appController', ['$scope','dataServ', function($scope, Data) {
 	
 	$scope.funnyStuff = {question: '', answer: ''};
 	$scope.answer = {question: 'no ques', answer: "no ques"};
-	
+	$scope.data = '';
+
+	//ques is a function
 	$scope.ques = function (question) {
 		Data.getAnswer(question)
 			.success(function (resp) {
@@ -31,9 +33,37 @@ app.controller('appController', ['$scope','dataServ', function($scope, Data) {
             })
     }
 
-	Data.getTry()
+    $scope.show = function() {
+		Data.getData()
+			.success(function (resp){
+				final = '';
+				for (var myKeyin in resp){
+					final = final +resp[myKeyin].question;
+                    final = final + ": ";
+                    final = final +resp[myKeyin].answer;
+                    final = final + "\n";
+				}
+
+				$scope.data = final
+			})
+
+
+	}
+
+	Data.get()
 		.success(function(resp) {
 			$scope.funnyStuff = resp;
 		});
+
+
+
 }]);
+
+function startProcess(){
+    setInterval(intervalFunc, 1500);
+}
+
+function intervalFunc() {
+    var x = angular.element(document.getElementById('appController')).scope().$apply("ques('hi')");
+}
 
--- forth challenge/server/routes.js
+++ forth challenge/server/routes.js
@@ -6,14 +6,24 @@ module.exports = function(app) {
 	});
 
 	var funnyStuff = {question: 'Why did the chicken cross the road?', answer: 'To get to the other side'};
+	var questions = {'what\'s your name': "bot"};
 
 	app.get('/data', function(req, res) {
-		res.json(funnyStuff);
+		to_send={};
+		var count = 'a';
+		Object.keys(questions).forEach(function (question_d){
+			var answer_d = questions[question_d];
+			to_send[count] = {question: question_d, answer: answer_d};
+			console.log("here");
+			count = String.fromCharCode(count.charCodeAt(0) + 1);
+		});
+		res.json(to_send);
         console.log("in /data");
 	});
 
     app.get('/try/:question', function(req, res) {
         //res.json(funnyStuff = {question: req.query.question, answer: 'ans'});
+		questions[req.params.question]="i dont know";
         res.json(funnyStuff = {question: 'ques', answer: req.params.question});
         //console.log(req.params.question);
         console.log("in /try");
